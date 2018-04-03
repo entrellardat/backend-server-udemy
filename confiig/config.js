cd4b4d1d310ec1ca0e6926981718dafef9edd074
Aramos <alejandro.entrellardat@gmail.com>
2018-04-03 18:33:34 +0200

    terminado el backend-server

--- confiig/config.js
+++ confiig/config.js
@@ -1,1 +1,5 @@
 module.exports.SEED = 'este-es-un-seed-dificil';
+
+//google
+module.exports.CLIENT_ID = '540139584127-gr3nvac6kujlmmaf8ahn9sf1f75esgla.apps.googleusercontent.com';
+module.exports.GOOGLE_SECRET = 'S4UDY10BvKlB_EnRvihMyH4l';
--- routes/login.js
+++ routes/login.js
@@ -1,11 +1,102 @@
+const { OAuth2Client } = require('google-auth-library');
+
 var express = require('express');
 var app = express();
 var bcrypt = require('bcryptjs');
 var jwt = require('jsonwebtoken');
 var SEED = require('../confiig/config').SEED;
-
 var Usuario = require('../models/Usuario');
+var CLIENT_ID = require('../confiig/config').CLIENT_ID;
+var GOOGLE_SECRET = require('../confiig/config').GOOGLE_SECRET;
 
+
+// ==================================================================
+// AUTENTICACION GOOGLE 
+// ==================================================================
+app.post('/google', (req, res) => {
+
+    const client = new OAuth2Client(CLIENT_ID, GOOGLE_SECRET);
+
+    var idToken = req.body.idToken;
+
+    const payload = client.verifyIdToken({
+        idToken: idToken,
+        audience: CLIENT_ID
+    });
+
+    payload.then(data => {
+        Usuario.findOne({ email: data.email }, (err, usuario) => {
+            if (err) {
+                return res.status(500).json({
+                    ok: 'false',
+                    mensaje: 'No existe el usuario en la base de datos',
+                    errors: err
+                });
+            }
+
+            if (usuario) {
+                if (!usuario.google) {
+                    return res.status(400).json({
+                        ok: false,
+                        mensaje: 'Debe usar su autenticacion normal',
+                        errors: err
+                    });
+                }
+
+                usuario.password = ':)';
+                var token = jwt.sign({ usuario: usuario }, SEED, { expiresIn: 14400000 });
+
+                return res.status(200).json({
+                    ok: true,
+                    usuario: usuario,
+                    token: token,
+                    id: usuario.id
+                });
+
+            } else {
+                // Si el usuario no existe por correo
+                var usuario = new Usuario();
+                usuario.nombre = data.payload.name;
+                usuario.email = data.payload.email;
+                usuario.role = 'ADMIN';
+                usuario.password = ':)';
+                usuario.img = data.payload.picture;
+                usuario.google = true;
+
+                usuario.save((err, usuarioDB) => {
+                    if (err) {
+                        return res.status(500).json({
+                            ok: 'false',
+                            mensaje: 'Error al crear usuario',
+                            errors: err
+                        });
+                    }
+
+                    var token = jwt.sign({ usuario: usuarioDB }, SEED, { expiresIn: 14400000 });
+                    return res.status(200).json({
+                        ok: true,
+                        mensaje: usuarioDB,
+                        token: token,
+                        id: usuarioDB.id
+                    });
+
+                });
+            }
+        });
+    }).catch(err => {
+        if (err) {
+            return res.status(400).json({
+                ok: false,
+                mensaje: 'Token no válido',
+                errors: err
+            });
+        }
+    });
+});
+
+// ==================================================================
+// AUTENTICACION NORAML 
+// ==================================================================
 app.post('/', (req, res) => {
 
     var body = req.body;
